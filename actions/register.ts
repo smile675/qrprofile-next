6eae57c29489abbc78e1613bdddd416424039df9
smile675 <angred.ismail@gmail.com>
2024-05-01 20:53:16 +0800

    called first prisma funciton , register user with neon database

--- actions/register.ts
+++ actions/register.ts
@@ -1,17 +1,42 @@
 "use server"
 
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs"
+
 import { RegisterSchema } from "@/schemas";
 
 import * as z from "zod"
+import { getUserbyEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema> )=>{
-    const validatedFields = RegisterSchema.safeParse(values);
+    const validatedFields =  RegisterSchema.safeParse(values);
     
-    if(!validatedFields){
+    if(!validatedFields.success){
         return {error: "Invalid fields"};
     }
 
+    const { email, password, name } = validatedFields.data;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const existingUser = await getUserbyEmail(email);
+
+    if(existingUser){
+        return {error: "This email already in use"}
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    });
+
+    //TODO send verification token email.
+    
+
     return {
-        success: "Success"
+        success: "User created"
     }
 }
