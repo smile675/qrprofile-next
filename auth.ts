62c3d08e8dc696346fe29b5510bc62711345aeed
smile675 <angred.ismail@gmail.com>
2024-05-02 20:53:27 +0800

    added custom field to session

--- auth.ts
+++ auth.ts
@@ -1,7 +1,19 @@
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import authConfig from "@/auth.config"
 import {PrismaAdapter} from "@auth/prisma-adapter"
 import {db} from "@/lib/db"
+
+import { getUserbyId } from "./data/user"
+import { PaymentStatus } from "@prisma/client"
+
+declare module "next-auth" {
+    interface Session {
+      user: {
+        paymentStatus: PaymentStatus
+      } & DefaultSession["user"]
+    }
+}
+
 
 export const {
     handlers: {GET, POST},
@@ -9,6 +21,32 @@ export const {
     signIn,
     signOut,
 } = NextAuth({
+    callbacks: {
+        
+        async session({token, session}){
+            // console.log({
+            //     sessonTOken: token,
+            // });
+            if(token.sub && session.user){
+                session.user.id = token.sub;
+            }
+            
+            if(token.paymentStatus && session.user){
+                
+                session.user.paymentStatus = token.paymentStatus;
+            }
+            // console.log({session});
+            return session;
+        },
+        async jwt({token}){
+            if(!token.sub) return token;
+            const existingUser = await getUserbyId(token.sub);
+            if(!existingUser) return token;
+            token.paymentStatus = existingUser.paymentStatus;
+            // console.log({token: token});
+            return token;
+        },
+    },
     adapter: PrismaAdapter(db),
     session: {strategy: "jwt"},
     ...authConfig
